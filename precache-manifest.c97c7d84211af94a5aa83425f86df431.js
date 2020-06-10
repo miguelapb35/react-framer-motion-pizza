0b1df93f1686da5353c6cca4c200780b787f80c8
Miguel Angel <miguelapb35@gmail.com>
2020-06-10 13:25:38 -0500

    Updates

---
+++ precache-manifest.c97c7d84211af94a5aa83425f86df431.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c877c91d804d5bba9e1dc9105f7ddaa",
+    "url": "/react-framer-motion-pizza/index.html"
+  },
+  {
+    "revision": "b7a9301210dad2389880",
+    "url": "/react-framer-motion-pizza/static/css/main.f0fd58af.chunk.css"
+  },
+  {
+    "revision": "d4b17a6782493bb53697",
+    "url": "/react-framer-motion-pizza/static/js/2.bfdb7452.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/react-framer-motion-pizza/static/js/2.bfdb7452.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7a9301210dad2389880",
+    "url": "/react-framer-motion-pizza/static/js/main.ce0be8e3.chunk.js"
+  },
+  {
+    "revision": "353e5bad68667e88331a",
+    "url": "/react-framer-motion-pizza/static/js/runtime-main.4ae7044a.js"
+  }
+]);
